2747488f126695d00bd782a66c9c45600a6e990d
Bridget Woodye <BrdgtColleenW@gmail.com>
2018-10-05 15:17:29 -0400

    taking the toolbar
    I commented out the full login work that attaches to the php so we can easily move between the login screens and the home
    main.js only deals with the toolbar and home pages, this is the same for the css

--- js/login.js
+++ js/login.js
@@ -7,76 +7,76 @@ function toRegPage()
 
 function doLogin()
 {
-	userId = 0;
-	firstName = "";
-	lastName = "";
-	lastIdClicked = 0;
-
-	var login = document.getElementById("loginName").value;
-	var password = md5(document.getElementById("loginPassword").value);
-
-	// sanitizing the login and Password
-	var i;
-	for(i = 0; i < login.length; i++)
-	{
-		if(login[i] === ';' || login[i] === '/' || login[i] === '-' || login[i] == ')' || login[i]=='(')
-		{
-			alert("There are illegal characters in your login");
-			document.getElementById("loginName").value = loginName.defaultValue;
-			document.getElementById("loginPassword").value =loginPassword.defaultValue;
-			return;
-		}
-
-		if(password[i] === ';' || password[i] === '/' || password[i] === '-'|| password[i] == ')' || password[i]=='(')
-		{
-			alert("There are illegal characters in your password.");
-			document.getElementById("loginName").value = loginName.defaultValue;
-			document.getElementById("loginPassword").value =loginPassword.defaultValue;
-			return;
-		}
-	}
-
-	document.getElementById("loginResult").innerHTML = "";
-
-	var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
-	var url = urlBase + '/Login.' + extension;
-
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, false);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.send(jsonPayload);
-
-		var jsonObject = JSON.parse( xhr.responseText );
-
-		userId = jsonObject.userID;
-
-		if( userId < 1 || userId == 'undefined' || userId == null)
-		{
-			document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
-			return;
-		}
-
-		firstName = jsonObject.firstName;
-		lastName = jsonObject.lastName;
-
-		document.getElementById("userName").innerHTML = firstName + " " + lastName;
-
-		document.getElementById("loginName").value = loginName.defaultValue;
-		document.getElementById("loginPassword").value =loginPassword.defaultValue;
-
-		document.getElementById("loggedInDiv").style.display = "inline";
-		document.getElementById("loggedInDiv").style.visibility = "visible";
-	//	hideOrShow( "loggedInDiv", true);
+	// userId = 0;
+	// firstName = "";
+	// lastName = "";
+	// lastIdClicked = 0;
+  //
+	// var login = document.getElementById("loginName").value;
+	// var password = md5(document.getElementById("loginPassword").value);
+  //
+	// // sanitizing the login and Password
+	// var i;
+	// for(i = 0; i < login.length; i++)
+	// {
+	// 	if(login[i] === ';' || login[i] === '/' || login[i] === '-' || login[i] == ')' || login[i]=='(')
+	// 	{
+	// 		alert("There are illegal characters in your login");
+	// 		document.getElementById("loginName").value = loginName.defaultValue;
+	// 		document.getElementById("loginPassword").value =loginPassword.defaultValue;
+	// 		return;
+	// 	}
+  //
+	// 	if(password[i] === ';' || password[i] === '/' || password[i] === '-'|| password[i] == ')' || password[i]=='(')
+	// 	{
+	// 		alert("There are illegal characters in your password.");
+	// 		document.getElementById("loginName").value = loginName.defaultValue;
+	// 		document.getElementById("loginPassword").value =loginPassword.defaultValue;
+	// 		return;
+	// 	}
+	// }
+  //
+	// document.getElementById("loginResult").innerHTML = "";
+  //
+	// var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
+	// var url = urlBase + '/Login.' + extension;
+  //
+	// var xhr = new XMLHttpRequest();
+	// xhr.open("POST", url, false);
+	// xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	// try
+	// {
+	// 	xhr.send(jsonPayload);
+  //
+	// 	var jsonObject = JSON.parse( xhr.responseText );
+  //
+	// 	userId = jsonObject.userID;
+  //
+	// 	if( userId < 1 || userId == 'undefined' || userId == null)
+	// 	{
+	// 		document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+	// 		return;
+	// 	}
+  //
+	// 	firstName = jsonObject.firstName;
+	// 	lastName = jsonObject.lastName;
+  //
+	// 	document.getElementById("userName").innerHTML = firstName + " " + lastName;
+  //
+	// 	document.getElementById("loginName").value = loginName.defaultValue;
+	// 	document.getElementById("loginPassword").value =loginPassword.defaultValue;
+  //
+	// 	document.getElementById("loggedInDiv").style.display = "inline";
+	// 	document.getElementById("loggedInDiv").style.visibility = "visible";
+	  hideOrShow( "tabs", true);
 		hideOrShow( "accessUIDiv", true);
-		hideOrShow( "loginDiv", false);
+		hideOrShow( "loginPage", false);
 
-	}
-	catch(err)
-	{
-		document.getElementById("loginResult").innerHTML = err.message;
-	}
+	// }
+	// catch(err)
+	// {
+	// 	document.getElementById("loginResult").innerHTML = err.message;
+	// }
 
 }
 
