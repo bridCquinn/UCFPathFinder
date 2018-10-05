763633e892ebde5aec6f3e0267086f9783a0ce40
Bridget Woodye <BrdgtColleenW@gmail.com>
2018-10-05 13:08:47 -0400

    Create sharedFunctions.js

---
+++ js/sharedFunctions.js
@@ -0,0 +1,20 @@
+var urlBase = 'http://ucfpathfinder.com/';
+var extension = "php";
+
+var userId = 0;
+var firstName = "";
+var lastName = "";
+
+function hideOrShow( elementId, showState )
+{
+	var vis = "visible";
+	var dis = "block";
+	if( !showState )
+	{
+		vis = "hidden";
+		dis = "none";
+	}
+
+	document.getElementById( elementId ).style.visibility = vis;
+	document.getElementById( elementId ).style.display = dis;
+}
