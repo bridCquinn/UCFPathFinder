5e9a2bfba6302775f020ae9b192a1be68f81fd31
Qclaudie <42715820+Qclaudie@users.noreply.github.com>
2018-10-11 12:26:26 -0400

    Added Refracted Login.php

---
+++ API/Login.php
@@ -0,0 +1,77 @@
+<?php
+/*
+Login:
+SELECT userID, firstName, lastName, login FROM users WHERE login = '<login>' AND password = '<hashed password>';
+*/
+	$inData = getRequestInfo();
+	
+	$userID = 0;
+	$firstName = "";
+	$lastName = "";
+	
+	/*Connection to the database*/
+	$conn = new mysqli("localhost", "root", "orlando", "ucfpathfinder");
+	
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		
+		$sql = "CALL login (?, ?)";
+		
+		if($stmt = $conn->prepare($sql))
+		{
+			/*creates the prepared statement*/
+			$stmt->bind_param('ss', $inData["login"], $inData["password"]);/*Binds params to markers*/
+			$stmt->execute();
+			$result	= $stmt->get_result();
+			//$result = $conn->query($sql);
+			if ($result->num_rows > 0)
+			{
+				$row = $result->fetch_assoc();
+				$firstName = $row["firstName"];
+				$lastName = $row["lastName"];
+				$userID = $row["userID"];
+				
+				$sql2 = "UPDATE users SET dateLastLoggedIn = NOW() WHERE userID = $userID";
+				$conn->query($sql2);
+				
+				
+			
+				returnWithInfo($firstName, $lastName, $userID);
+			}
+			
+			else
+			{
+				returnWithError( "No Records Found" );
+			}
+		}
+		
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"userID":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $firstName, $lastName, $userID)
+	{
+		$retValue = '{"userID":' . $userID . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
