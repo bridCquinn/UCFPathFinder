8d8c0c556ac61b909fda52f64330c8642bf3ddb3
Bridget Woodye <BrdgtColleenW@gmail.com>
2018-10-12 14:37:43 -0400

    schedule builder
    fixing the set up of the list of classes
    the text in each class is now invisible when collapsed

--- js/schedule.js
+++ js/schedule.js
@@ -1,3 +1,12 @@
 function newSchedule() {
   document.getElementById('newSch').style.display = 'block';
 }
+
+function toggle(elementId){
+  var x = document.getElementById(elementId);
+    if (x.style.visibility === "visible") {
+        x.style.visibility = "hidden";
+    } else {
+        x.style.visibility = "visible";
+    }
+}
