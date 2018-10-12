6a53231c910ff805c28bf3a2e6627f5ec0f89463
Bridget Woodye <BrdgtColleenW@gmail.com>
2018-10-12 16:05:52 -0400

    schedule maker
    Deisgning modal more
    created addClass function

--- js/schedule.js
+++ js/schedule.js
@@ -1,3 +1,33 @@
+
+function Course(){
+  this.className = "";
+  this.classCode = "";
+  this.startTime = "";
+  this.endTime = "";
+  this.building = "";
+  this.notes = "";
+};
+
+var scheduleList = [];
+
+function addClass(){
+  var course = new Course();
+
+  course.className = document.getElementById("code").value;
+  course.classCode = document.getElementById("name").value;
+  course.startTime = document.getElementById("start").value;
+  course.endTime = document.getElementById("end").value;
+  course.building = document.getElementById("address").value;
+  course.notes = document.getElementById("note").value;
+
+  scheduleList.push(course);
+
+  // alert(scheduleList[0].className + "," + course.classCode+ "," +course.startTime+ "," +course.endTime+ "," +course.building+ "," +course.notes);
+
+  // need to make sure the list is updates properly
+  // the scroll bar needs to be put in
+}
+
 function newSchedule() {
   document.getElementById('newSch').style.display = 'block';
 }
