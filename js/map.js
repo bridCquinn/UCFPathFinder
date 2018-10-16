c6b0722aaf9659c3bc54515ed890f775bfac4041
Ayleen <islandordaz@gmail.com>
2018-10-15 20:14:05 -0400

    commented out some things for testing

--- js/map.js
+++ js/map.js
@@ -2,10 +2,10 @@ function myMap()
 {
 	hideOrShow("mapUIDiv", true);
 	
-	var mapOptions = {
-		center: new google.maps.LatLng(51.5, -0.12),
-		zoom: 10,
-		mapTypeId: google.maps.MapTypeId.HYBRID
-	}
-	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	//var mapOptions = {
+	//	center: new google.maps.LatLng(51.5, -0.12),
+	//	zoom: 10,
+	//	mapTypeId: google.maps.MapTypeId.HYBRID
+	//}
+	//var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
