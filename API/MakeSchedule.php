b057a2c3f6deb2b86fbc277579d85896e320997c
dkalley <42875343+dkalley@users.noreply.github.com>
2018-11-09 15:45:15 -0500

    Update MakeSchedule.php

--- API/MakeSchedule.php
+++ API/MakeSchedule.php
@@ -9,22 +9,8 @@
 */
 
 	$inData = getRequestInfo();
-	$temp = 15;
-	$temp1 = 1;
-	$temp2 = "test";
-	$temp3 = "12:00:00";
-	$temp4 = "13:15:00";
-	$temp5 = "test1234";
-	$temp6 = "fall";
-	$temp7 = 2018;
-	$temp8 = "test test test";
 //    	$userID = $inData["userID"];
 //    	$array  = $inData["schedule"];
-
-//	$userID = 15;
-//    	$array = [0,"test","","","test","test",0,"test"];    
-//      $array = [0,"test","","","test","test",0,"test",
-//              1,"test1","","","test1","test1",1,"test1"];
 
 	echo("HERE");
 //    	$length = count($array);
@@ -44,17 +30,13 @@
 		
        	    if($stmt = $conn->prepare($sql))
             {
-		    
-		//   $stmt->bind_param('iisssssis', $temp,$temp1,$temp2,$temp3,$temp4,$temp5,$temp6,$temp7,$temp8 );
 
-            	//    $stmt->execute();
-            	//    $result = $stmt->get_result();
 		    /*
-                for($i = 0; $i < ($length % 8); $i++) { 
-            	    $stmt->bind_param('iisssssis', $userID, $array[$i*8],
-                        $array[$i*8+1],$array[$i*8+2],$array[$i*8+3],
-                        $array[$i*8+4],$array[$i*8+5],$array[$i*8+6],
-                        $array[$i*8+7]);
+                for($i = 0; $i < $length; $i++) { 
+            	    $stmt->bind_param('iisssssis', $userID, $array[?],
+                        $array[?],$array[?],$array[?],
+                        $array[?],$array[?],$array[?],
+                        $array[?]);
 
             	    $stmt->execute();
             	    $result = $stmt->get_result();
