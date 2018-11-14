a128c0916b77994f2e446fbefd9aab07faf723b5
dkalley <42875343+dkalley@users.noreply.github.com>
2018-11-14 15:29:23 -0500

    Update SearchBuildings.php

--- API/SearchBuildings.php
+++ API/SearchBuildings.php
@@ -66,12 +66,13 @@
 		}
 		$conn->close();
 	}
-	
+*/
+
 	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
-
+/*
 	function sendResultInfoAsJson( $obj )
 	{
 		header('Content-type: application/json');
