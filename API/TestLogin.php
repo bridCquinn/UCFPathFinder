4ef17d3ed26fff0b42da740d45123038801f8f61
dkalley <42875343+dkalley@users.noreply.github.com>
2018-11-15 15:35:02 -0500

    Create TestLogin.php

---
+++ API/TestLogin.php
@@ -0,0 +1,82 @@
+<?php
+/*
+Login:
+SELECT userID, firstName, lastName, login FROM users WHERE login = '<login>' AND password = '<hashed password>';
+*/
+	$inData = getRequestInfo();
+	
+	$userID = 0;
+	$firstName = "";
+	$lastName = "";
+	
+	/*Connection to the database*/
+	$conn = new mysqli("localhost", "root", "orlando", "ucfpathfinder");
+	
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		
+		$sql = "CALL login (?, ?)";
+		
+		if($stmt = $conn->prepare($sql))
+		{
+			/*creates the prepared statement*/
+			//$stmt->bind_param('ss', $inData["username"], $inData["password"]);/*Binds params to markers*/
+			$stmt->bind_param('ss', $name, $name);/*Binds params to markers*/
+			$name = "bobby";
+			$stmt->execute();
+			$result	= $stmt->get_result();
+			//$result = $conn->query($sql);
+			if ($result->num_rows > 0)
+			{
+				$row = $result->fetch_assoc();
+				$firstName = $row["firstName"];
+				$lastName = $row["lastName"];
+				$userID = $row["userID"];
+				
+				/*
+				$sql2 = "UPDATE users SET dateLastLoggedIn = NOW() WHERE userID = $userID";
+				$conn->query($sql2);
+				*/
+				// TESTING
+				$call = "php GetSchedule.php fall 2018 ".$userID;
+				$schedule = shell_exec($call);
+			  echo $schedule;
+				//returnWithInfo($firstName, $lastName, $userID);
+			}
+			
+			else
+			{
+				returnWithError( "No Records Found" );
+			}
+		}
+		
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"userID":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $firstName, $lastName, $userID, $schedule)
+	{
+		$retValue = '{"userID":' . $userID . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
