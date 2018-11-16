535d28d0c11427ad68ceef46721ac978ede6915e
dkalley <42875343+dkalley@users.noreply.github.com>
2018-11-15 19:45:04 -0500

    testing

--- js/login.js
+++ js/login.js
@@ -67,6 +67,8 @@ function doLogin()
 		document.getElementById("loginName").value = loginName.defaultValue;
 		document.getElementById("loginPassword").value =loginPassword.defaultValue;
 
+		alert(jsonObject.schedule[0]);
+		
     hideOrShow( "tabs", true);
 		hideOrShow( "accessUIDiv", true);
 		hideOrShow( "loginPage", false);
