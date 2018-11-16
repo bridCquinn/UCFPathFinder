b2d74823db6b2f1ea9f8138b86c3cfc7b5bc7228
dkalley <42875343+dkalley@users.noreply.github.com>
2018-11-15 19:47:45 -0500

    Update TestLogin.php

--- API/TestLogin.php
+++ API/TestLogin.php
@@ -24,7 +24,9 @@ SELECT userID, firstName, lastName, login FROM users WHERE login = '<login>' AND
 		if($stmt = $conn->prepare($sql))
 		{
 			/*creates the prepared statement*/
-			$stmt->bind_param('ss', $inData["username"], $inData["password"]);/*Binds params to markers*/
+			//$stmt->bind_param('ss', $inData["username"], $inData["password"]);/*Binds params to markers*/
+			$stmt->bind_param('ss', $name, $name);
+			$name = 'bobby';
 			$stmt->execute();
 			$result	= $stmt->get_result();
 			//$result = $conn->query($sql);
