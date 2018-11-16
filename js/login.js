de8204d4b47f07cb07d870157ebde88dc9d52761
dkalley <42875343+dkalley@users.noreply.github.com>
2018-11-15 20:23:11 -0500

    removing testing lines

--- js/login.js
+++ js/login.js
@@ -66,8 +66,6 @@ function doLogin()
 
 		document.getElementById("loginName").value = loginName.defaultValue;
 		document.getElementById("loginPassword").value =loginPassword.defaultValue;
-
-		alert(jsonObject.schedule[0]);
 		
     hideOrShow( "tabs", true);
 		hideOrShow( "accessUIDiv", true);
