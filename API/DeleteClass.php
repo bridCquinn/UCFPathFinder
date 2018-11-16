060ebf76ada831333f27304b8298846009427a84
dkalley <42875343+dkalley@users.noreply.github.com>
2018-11-16 14:09:28 -0500

    Update DeleteClass.php

--- API/DeleteClass.php
+++ API/DeleteClass.php
@@ -1,6 +1,5 @@
 <?php
-	/*
-    
+/*
    		JSON package expected
     		{ 
       			"userID"    :  <<userID>>
@@ -10,9 +9,9 @@
     		}
 		
         	Deletes class associated with that userID classCode term and year
-    	*/
-
+ */
 	$inData = getRequestInfo();
+
 	$conn = new mysqli("localhost", "root", "orlando", "ucfpathfinder");
 	
 	if ($conn->connect_error) 
@@ -43,6 +42,7 @@
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
+
 	function sendResultInfoAsJson( $obj )
 	{
 		header('Content-type: application/json');
