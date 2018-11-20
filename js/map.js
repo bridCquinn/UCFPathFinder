1a69a888738dce297a1f9ee04f1a11adfd487596
Unknown <sscooter52@gmail.com>
2018-11-20 14:17:47 -0500

    testing another UL fix

--- js/map.js
+++ js/map.js
@@ -16,7 +16,7 @@ function myMap()
 
 function searchLocation()
 {
-	var thisUL = getElementById("mapUL");
+	var thisUL = document.getElementById("mapUL");
 	while(thisUL.hasChildNodes())
 	{
 		// myUL.removeChild(mapUL.firstChild);
