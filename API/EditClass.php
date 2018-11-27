f244f1978c9c33b90a9fd16f27a1937c6f2ce8f7
dkalley <42875343+dkalley@users.noreply.github.com>
2018-11-27 16:59:40 -0500

    removed hard coded user and pass

--- API/EditClass.php
+++ API/EditClass.php
@@ -14,8 +14,10 @@
 	$inData = getRequestInfo();
     	$userID = $inData["userID"];
     	$class  = $inData["class"];
-	
-	$conn = new mysqli("localhost", "root", "orlando", "ucfpathfinder");
+
+	$info = json_decode(file_get_contents('info.json'), true);
+
+	$conn = new mysqli("localhost", $info["name"], $info["pass"], $info["data"]);	
 
 	if ($conn->connect_error) 
 	{	
