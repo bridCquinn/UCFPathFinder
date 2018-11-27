dd8a483bdabc3f92b47f57fdafdac2b48adb4ece
Vinny <vbrusco30794@gmail.com>
2018-11-27 12:01:52 -0500

    Map Working

--- UCFPathFinder/app/src/main/java/com/ucfpathfinder/ucfpathfinder/MapsActivity.java
+++ UCFPathFinder/app/src/main/java/com/ucfpathfinder/ucfpathfinder/MapsActivity.java
@@ -17,7 +17,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_maps2);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
