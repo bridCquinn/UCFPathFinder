3178494d4641549c761852c26ccfe42fd9d2c632
dkalley <42875343+dkalley@users.noreply.github.com>
2018-11-27 17:02:07 -0500

    Update Register.php

--- API/Register.php
+++ API/Register.php
@@ -11,8 +11,10 @@
 */
 	$inData = getRequestInfo();
 	
-	$conn = new mysqli("localhost", "root", "orlando", "ucfpathfinder");
+	$info = json_decode(file_get_contents('info.json'), true);
 	
+	$conn = new mysqli("localhost", $info["name"], $info["pass"], $info["data"]);	
+		
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
