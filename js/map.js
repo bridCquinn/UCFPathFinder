17ee387855e03eb1b0a5814adf09f63fcd815dc2
Ayleen <islandordaz@gmail.com>
2018-11-28 04:15:38 -0500

    testing

--- js/map.js
+++ js/map.js
@@ -55,7 +55,7 @@ function searchLocation()
 						var span = document.createElement("SPAN");
 						var txt = document.createTextNode("\u00D7");
 						li.id = jsonObject['results'][i][0];
-					//	li.setAttribute('onclick', "displayInfo(this.id)");
+						li.setAttribute('onclick', "setBuildingTo(this.id)"); // does nothing right now
 						span.appendChild(txt);
 					
 					//	if(document.getElementById("div" + li.id) != null)
@@ -78,69 +78,6 @@ function searchLocation()
 	
 }
 
-function testSearchBuilding()
-{
-	while(mapUL.hasChildNodes())
-	{
-		mapUL.removeChild(mapUL.childNodes[0]);
-	}
-	
-		var srch = document.getElementById("testDropdownInput").value;
-		
-		var jsonPayload =  '{"search" : "' + srch + '"}';
-		var url = urlBase + '/SearchBuildings.' + extension;
-		
-		var xhr = new XMLHttpRequest();
-		xhr.open("POST", url, true);
-		xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-		try
-		{
-			xhr.onreadystatechange = function()
-			{
-				if(this.readyState == 4 && this.status == 200)
-				{
-					var jsonObject = JSON.parse(xhr.responseText);
-					var i;
-					//var array = JSON.stringify(jsonObject[result"][1]);
-					var x = "";
-				
-				
-					
-					for(i=0; i < jsonObject['results'].length ; i++)
-					{
-						x = jsonObject['results'][i][2];
-						//console.log(x);
-						var a = document.createElement("a");
-						var t = document.createTextNode(JSON.stringify(x));
-						
-						a.appendChild(t);
-						document.getElementById("testDropdownInput").appendChild(a);
-						
-						var span = document.createElement("SPAN");
-						var txt = document.createTextNode("\u00D7");
-						a.id = jsonObject['results'][i][0];
-						a.setAttribute('onclick', "setBuildingTo(this.id)");
-						span.appendChild(txt);
-					
-					//	if(document.getElementById("div" + li.id) != null)
-					//	{
-						//	continue;
-					//	}
-						
-					
-					
-					}
-					
-				}
-			};
-			xhr.send(jsonPayload);
-		}
-		catch(err)
-		{
-			;
-		}
-	
-}
 
 function setBuildingTo()
 {
