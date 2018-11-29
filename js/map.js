f36969632fd3c8105eb159a722d0e4209d1dd404
Ayleen <islandordaz@gmail.com>
2018-11-29 12:35:49 -0500

    testing search function

--- js/map.js
+++ js/map.js
@@ -3,14 +3,14 @@
 function myMap()
 {
 	lastSearchLocationClicked = 0;
-	
+
 	//var mapOptions = {
 	//	center: new google.maps.LatLng(51.5, -0.12),
 	//	zoom: 10,
 	//	mapTypeId: google.maps.MapTypeId.HYBRID
 	// curly bracket goes here
 	//var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-	
+
 	searchLocation();
 }
 
@@ -20,15 +20,15 @@ function searchLocation()
 	{
 		mapUL.removeChild(mapUL.childNodes[0]);
 	}
-	
+
 		var srch = document.getElementById("searchText").value;
-		
+
 		if(srch == "")
 			return;
-		
+
 		var jsonPayload =  '{"search" : "' + srch + '"}';
 		var url = urlBase + '/SearchBuildings.' + extension;
-		
+
 		var xhr = new XMLHttpRequest();
 		xhr.open("POST", url, true);
 		xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
@@ -42,34 +42,34 @@ function searchLocation()
 					var i;
 					//var array = JSON.stringify(jsonObject[result"][1]);
 					var x = "";
-				
-				
-					
+
+
+
 					for(i=0; i < jsonObject['results'].length ; i++)
 					{
 						x = jsonObject['results'][i][2];
 						//console.log(x);
 						var li = document.createElement("li");
 						var t = document.createTextNode(JSON.stringify(x));
-						
+
 						li.appendChild(t);
 						document.getElementById("mapUL").appendChild(li);
-						
+
 						var span = document.createElement("SPAN");
 						var txt = document.createTextNode("\u00D7");
 						li.id = jsonObject['results'][i][0];
 						li.setAttribute('onclick', "setBuildingTo(this.id)"); // does nothing right now
 						span.appendChild(txt);
-					
+
 					//	if(document.getElementById("div" + li.id) != null)
 					//	{
 						//	continue;
 					//	}
-						
-					
-					
+
+
+
 					}
-					
+
 				}
 			};
 			xhr.send(jsonPayload);
@@ -78,14 +78,15 @@ function searchLocation()
 		{
 			;
 		}
-	
+
 }
 
 
-function setBuildingTo()
+function setBuildingTo(location)
 {
-	
-	;
+
+ 	document.getElementById("searchText").value =  location;
+
 }
 
 
@@ -100,7 +101,7 @@ function displayInfo(location)
 		document.getElementById("div" + lastSearchLocationClicked).style.display = "none";
 		document.getElementById("div" + lastSearchLocationClicked).style.visibility = "hidden";
 	}
-	
+
 	document.getElementById("div" + location).style.display = "block";
 	document.getElementById("div" + location).style.visibility = "visible";
 
