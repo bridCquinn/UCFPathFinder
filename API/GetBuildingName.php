5663344a2cb0c29c4ebdb3638151641d5c50c209
dkalley <42875343+dkalley@users.noreply.github.com>
2018-12-03 18:21:20 -0500

    Update GetBuildingName.php

--- API/GetBuildingName.php
+++ API/GetBuildingName.php
@@ -90,7 +90,7 @@
 
     function returnWithInfo( $searchResults )
 	{
-		$retValue = '{"results": ' . $searchResults . ' ,"error":""}';
+		$retValue = '{"results": "' . $searchResults . '" ,"error":""}';
 		sendResultInfoAsJson( $retValue );
 	}
 ?>
