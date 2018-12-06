381f9853b7ce1e2ccdbf106257caf630f6a4d589
Vinny <vbrusco30794@gmail.com>
2018-12-06 09:37:50 -0500

    final update

--- UCFPathFinder/app/src/main/java/com/ucfpathfinder/ucfpathfinder/MainActivity.java
+++ UCFPathFinder/app/src/main/java/com/ucfpathfinder/ucfpathfinder/MainActivity.java
@@ -129,7 +129,6 @@ public class MainActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
